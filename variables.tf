fc12e7d4cdbc28203db602a7621f119540f64308
rakesh1617 <rakeshsm709@gmail.com>
2024-08-14 23:42:08 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "aws_region" {
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-0ad21ae1d0696ad58"
 }
 
 variable "instance_type" {
